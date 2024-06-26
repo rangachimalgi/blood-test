c6e58b151e087cc7294ccb4c2b58a46febb4bd73
Ranga <rangachimalgi123@gmail.com>
2024-06-26 13:24:55 +0530

    commented the health checkups for men and women

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,24 @@
 import { Fragment, useContext, useEffect } from "react";
 import Wrapper from "../components/wrapper/Wrapper";
 import Section from "../components/Section";
-import { products, discoutProducts, popularTests, healthConcerns, checkupsMen, checkupsWomen } from "../utils/products";
+import {
+  products,
+  discoutProducts,
+  popularTests,
+  healthConcerns,
+  checkupsMen,
+  checkupsWomen,
+} from "../utils/products";
 import { DataContainer } from "../App";
 import SliderHome from "../components/Slider";
 import HealthConcernsSection from "../components/HealthConcernsSection.jsx";
 
 const Home = () => {
   const { addToCart } = useContext(DataContainer);
-  const newArrivalData = products.filter(item => item.category === "mobile" || item.category === "wireless");
-  const bestSales = products.filter(item => item.category === "Blood");
+  const newArrivalData = products.filter(
+    (item) => item.category === "mobile" || item.category === "wireless"
+  );
+  const bestSales = products.filter((item) => item.category === "Blood");
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -19,11 +28,27 @@ const Home = () => {
     <Fragment>
       <SliderHome />
       <Wrapper />
-      <HealthConcernsSection title="Recommended checkups for men" bgColor="#f6f9fc" productItems={checkupsMen} addToCart={addToCart} />
-      <HealthConcernsSection title="Recommended checkups for women" bgColor="#f6f9fc" productItems={checkupsWomen} addToCart={addToCart} />
-      <HealthConcernsSection title="Browse by Health Concerns" bgColor="#f6f9fc" productItems={healthConcerns} addToCart={addToCart} />
-      <Section id="popular-packages" title="Popular Packages" bgColor="#f6f9fc" productItems={discoutProducts} addToCart={addToCart} />
-      <Section title="Popular Tests" bgColor="#f6f9fc" productItems={popularTests} addToCart={addToCart} />
+      {/* <HealthConcernsSection title="Recommended checkups for men" bgColor="#f6f9fc" productItems={checkupsMen} addToCart={addToCart} />
+      <HealthConcernsSection title="Recommended checkups for women" bgColor="#f6f9fc" productItems={checkupsWomen} addToCart={addToCart} /> */}
+      <HealthConcernsSection
+        title="Browse by Health Concerns"
+        bgColor="#f6f9fc"
+        productItems={healthConcerns}
+        addToCart={addToCart}
+      />
+      <Section
+        id="popular-packages"
+        title="Popular Packages"
+        bgColor="#f6f9fc"
+        productItems={discoutProducts}
+        addToCart={addToCart}
+      />
+      <Section
+        title="Popular Tests"
+        bgColor="#f6f9fc"
+        productItems={popularTests}
+        addToCart={addToCart}
+      />
     </Fragment>
   );
 };
