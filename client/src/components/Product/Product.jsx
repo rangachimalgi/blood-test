4fa0b1fe9e0a6f2c59a23515385993ca3a95e5a1
Ranga <rangachimalgi123@gmail.com>
2024-07-30 03:35:08 +0530

    changes in the tests design

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -28,75 +28,93 @@ const Product = ({ title, productItem, addToCart, showImage = true, desc, enable
     toast.success("Product has been added to cart!");
   };
 
-  return (
-    <Col
-      lg={3}
-      md={4}
-      sm={6}
-      xs={12}
-      className={`product mtop ${enableHoverEffect ? 'hover-enabled' : ''}`}
-    >
-      {title === "Big Discount" && (
-        <span className="discount">{productItem.discount}% Off</span>
-      )}
-      <div className="product-image-container">
-        {showImage && (
-          <img
-            loading="lazy"
-            onClick={handleClick}
-            src={productItem.imgUrl}
-            alt={productItem.productName}
-            className="product-image"
-          />
-        )}
-        {enableHoverEffect && (
-          <div className="hover-overlay">
-            <h2>{productItem.overlayTitle}</h2>
-            <ul>
-              {productItem.overlayDetails?.map((detail, index) => (
-                <li key={index}>{detail}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-      {desc && <p className="product-description">{desc}</p>}
-      <div className="product-like">
-        <label>{count}</label> <br />
-        <ion-icon name="heart-outline" onClick={increment}></ion-icon>
-      </div>
-      <div className="product-details">
-        <h3 onClick={handleClick}>{productItem.productName}</h3>
-        <div className="price">
-          <h4>&#8377;{productItem.price}</h4>
-          <button
-            aria-label="Add"
-            type="submit"
-            className={`add ${isShopList ? 'shop-list-add-button' : ''}`}
-            onClick={handleAddToCart}
-          >
-            <ion-icon name="add"></ion-icon>
-          </button>
+  if (isShopList) {
+    return (
+      <Col
+        lg={12}
+        md={12}
+        sm={12}
+        xs={12}
+        className="product-list-item"
+      >
+        <div className="product-info">
+          <h3 onClick={handleClick}>{productItem.productName}</h3>
+          <p>Lab: {productItem.labName}</p>
         </div>
-        {isShopList && (
-          <div className="shop-list-buttons">
+        <div className="product-price">
+          <h4>&#8377;{productItem.price}</h4>
+          <div className="product-buttons">
             <button
-              className="shop-list-book-button"
+              className="product-button book-now"
               onClick={handleClick}
             >
               Book Now
             </button>
             <button
-              className="shop-list-add-to-cart-button"
+              className="product-button add-to-cart"
               onClick={handleAddToCart}
             >
               Add To Cart
             </button>
           </div>
+        </div>
+      </Col>
+    );
+  } else {
+    return (
+      <Col
+        lg={3}
+        md={4}
+        sm={6}
+        xs={12}
+        className={`product mtop ${enableHoverEffect ? 'hover-enabled' : ''}`}
+      >
+        {title === "Big Discount" && (
+          <span className="discount">{productItem.discount}% Off</span>
         )}
-      </div>
-    </Col>
-  );
+        <div className="product-image-container">
+          {showImage && (
+            <img
+              loading="lazy"
+              onClick={handleClick}
+              src={productItem.imgUrl}
+              alt={productItem.productName}
+              className="product-image"
+            />
+          )}
+          {enableHoverEffect && (
+            <div className="hover-overlay">
+              <h2>{productItem.overlayTitle}</h2>
+              <ul>
+                {productItem.overlayDetails?.map((detail, index) => (
+                  <li key={index}>{detail}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+        {desc && <p className="product-description">{desc}</p>}
+        <div className="product-like">
+          <label>{count}</label> <br />
+          <ion-icon name="heart-outline" onClick={increment}></ion-icon>
+        </div>
+        <div className="product-details">
+          <h3 onClick={handleClick}>{productItem.productName}</h3>
+          <div className="price">
+            <h4>&#8377;{productItem.price}</h4>
+            <button
+              aria-label="Add"
+              type="submit"
+              className={`add ${isShopList ? 'shop-list-add-button' : ''}`}
+              onClick={handleAddToCart}
+            >
+              <ion-icon name="add"></ion-icon>
+            </button>
+          </div>
+        </div>
+      </Col>
+    );
+  }
 };
 
 export default Product;
