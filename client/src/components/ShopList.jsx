82fe1712c16c442e12bee681901ec984638dbbef
Ranga <rangachimalgi123@gmail.com>
2024-08-29 16:41:41 +0530

    removed pagination in shoplist (Tests page)

--- client/src/components/ShopList.jsx
+++ client/src/components/ShopList.jsx
@@ -1,5 +1,5 @@
-import { Row, Pagination } from "react-bootstrap";
-import { useEffect, useState, useContext } from "react";
+import { Row } from "react-bootstrap";
+import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,13 +8,6 @@ import { DataContainer } from "../App";
 import "../Styles/Shop.css";
 
 const ShopList = ({ productItems }) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 50; // Change as needed
-  const totalPages = Math.ceil(productItems.length / itemsPerPage);
-  const displayedProducts = productItems.slice(
-    (currentPage - 1) * itemsPerPage,
-    currentPage * itemsPerPage
-  );
   const { addToCart } = useContext(DataContainer);
   const navigate = useNavigate();
 
@@ -59,7 +52,7 @@ const ShopList = ({ productItems }) => {
       <div>
         <ToastContainer />
         <Row className="justify-content-center">
-          {displayedProducts.map((productItem) => {
+          {productItems.map((productItem) => {
             return (
               <Product
                 key={productItem.id}
@@ -73,29 +66,6 @@ const ShopList = ({ productItems }) => {
             );
           })}
         </Row>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <Pagination>
-            <Pagination.Prev
-              onClick={() => setCurrentPage((old) => Math.max(old - 1, 1))}
-              disabled={currentPage === 1}
-            />
-            {[...Array(totalPages)].map((_, idx) => (
-              <Pagination.Item
-                key={idx}
-                active={idx + 1 === currentPage}
-                onClick={() => setCurrentPage(idx + 1)}
-              >
-                {idx + 1}
-              </Pagination.Item>
-            ))}
-            <Pagination.Next
-              onClick={() =>
-                setCurrentPage((old) => Math.min(old + 1, totalPages))
-              }
-              disabled={currentPage === totalPages}
-            />
-          </Pagination>
-        </div>
       </div>
     );
   }
